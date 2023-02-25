99d4f16e930fa0ace347579a8d46f15ba3809a7c
Jorge Felico <jorgefelico@gmail.com>
2023-02-24 21:16:32 -0500

    Switched api token to env variable

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Form from "react-bootstrap/Form";
 
 export default function Home() {
     const [restaurantResults, setRestaurantResults] = useState([]);
-
+    console.log(process.env.REACT_APP_TESTya)
     useEffect(() => {
         // navigator.geolocation.getCurrentPosition(function(position) {
         //   // console.log("Latitude is :", position.coords.latitude);
@@ -88,7 +88,7 @@ export default function Home() {
                 <div key={restaurant.id} className="bg-white font-semibold rounded-md border shadow-lg w-full">
                    {!process.env.NODE_ENV || process.env.NODE_ENV === 'development' ?
                     <img className="w-full rounded-t-md mx-auto" src={`https://picsum.photos/300/150?random=${restaurant.id}`} alt="" />:
-                    <img className="w-full rounded-t-md mx-auto" src={`https://maps.googleapis.com/maps/api/streetview?key=AIzaSyBzrk09mIwwMT-rgBZFKY1f4KDJadsKhZQ&location=${restaurant.name} ${restaurant.address} ${restaurant.city}&size=300x200`} alt="" />}
+                    <img className="w-full rounded-t-md mx-auto" src={`https://maps.googleapis.com/maps/api/streetview?key=${process.env.REACT_APP_GCP_API}&location=${restaurant.name} ${restaurant.address} ${restaurant.city}&size=300x200`} alt="" />}
                     <div className="p-4">
                         <h1 className="text-lg text-gray-700 text-center">{restaurant.name}</h1>
                         <h3 className="text-sm text-gray-400 text-center">{restaurant.address}</h3>
