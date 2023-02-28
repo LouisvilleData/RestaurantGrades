3e78ecdf1ca4ba76c7b460d210c3b05c7f014506
Jorge Felico <jorgefelico@gmail.com>
2023-02-27 19:19:05 -0500

    Testing GHPages

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Table from 'react-bootstrap/Table';
 // import Button from "react-bootstrap/Button";
 
